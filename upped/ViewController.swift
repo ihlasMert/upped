3ad5e01b1f16c42fef6e8ec1c9168eb0a7f1d72a
İhlas Mert <ihlas1mert@gmail.com>
2021-11-26 20:29:44 +0300

    add view will appear

--- upped/ViewController.swift
+++ upped/ViewController.swift
@@ -13,7 +13,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    override func didReceiveMemoryWarning() {
+        <#code#>
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        <#code#>
+    }
 }
 
